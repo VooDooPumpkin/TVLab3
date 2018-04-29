a513d31d60874a0c997798628ae2bcf80e04a6b2
Семён Колодин <samweanchester@gmail.com>
2018-04-29 17:50:06 +0300

    added changing by selected formula

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import sys
 import math
-import combinatorics.py
+import combinatorics
 from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 from PyQt5.QtWidgets import QDialog, QApplication, QMessageBox
@@ -17,6 +17,62 @@ class AppWindow(QDialog):
         self.ui = Ui_Dialog()
         self.ui.setupUi(self)
         self.show()
+
+        # connects
+        self.ui.chooseFormula.activated[int].connect(self.formulaChanged)
+
+    #Смена формулы
+    def formulaChanged(self, idx):
+        #show widgets for Bernuli
+        if idx == 0:
+            # show widgets for Bernuli
+            self.ui.groupBern.show()
+            self.ui.label_6.show()
+            self.ui.label_7.show()
+            self.ui.label_8.show()
+            self.ui.label_9.show()
+            self.ui.label_10.show()
+            self.ui.bernP.show()
+            self.ui.bernN.show()
+            self.ui.bernM.show()
+            self.ui.bernM1.show()
+            self.ui.bernM2.show()
+            # hide widgets for Polinomal
+            self.ui.label_11.hide()
+            self.ui.label_12.hide()
+            self.ui.label_13.hide()
+            self.ui.label_14.hide()
+            self.ui.poliN.hide()
+            self.ui.poliK.hide()
+            self.ui.poliM.hide()
+            self.ui.poliP.hide()
+
+        #show widgets for Polinomal
+        else:
+            # hide widgets for Bernuli
+            self.ui.groupBern.hide()
+            self.ui.groupBern.hide()
+            self.ui.label_6.hide()
+            self.ui.label_7.hide()
+            self.ui.label_8.hide()
+            self.ui.label_9.hide()
+            self.ui.label_10.hide()
+            self.ui.bernP.hide()
+            self.ui.bernN.hide()
+            self.ui.bernM.hide()
+            self.ui.bernM1.hide()
+            self.ui.bernM2.hide()
+            # show widgets for Polinomal
+            self.ui.label_11.show()
+            self.ui.label_12.show()
+            self.ui.label_13.show()
+            self.ui.label_14.show()
+            self.ui.poliN.show()
+            self.ui.poliK.show()
+            self.ui.poliM.show()
+            self.ui.poliP.show()
+
+
 
 # show window
 app = QApplication(sys.argv)
