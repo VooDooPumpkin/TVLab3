2990094df79570ebf85d24366ffe946d51d78d5b
Семён Колодин <samweanchester@gmail.com>
2018-04-30 12:16:58 +0300

    added muavra, needs tests and pictures

--- main.py
+++ main.py
@@ -22,6 +22,23 @@ def polinomal(n, k, arrayM, arrayP):
         res*= float(arrayP[i]) ** int(arrayM[i])
     return res
 
+#Integral Muavra formula
+def integralMuavra(n, p, m1, m2, segments):
+    q = 1 - p
+    x1 = (m1 - n*p)/math.sqrt(n*p*q)
+    x2 = (m2 - n*p)/math.sqrt(n*p*q)
+    h = (x2 - x1) / segments
+    res = 1/math.sqrt(2*math.pi)
+    integ = math.exp(-(x1**2/2))+ math.exp(-(x2**2/2))
+    for i in range(1,segments):
+        if (i%2 == 0):
+            integ += 2*(math.exp(-((x1 + i*h)**2/2)))
+        else:
+            integ += 4*(math.exp(-((x1 + i * h)**2/2)))
+    integ *= h/3
+    res *= integ
+    return res
+
 # window
 class AppWindow(QDialog):
 
@@ -38,6 +55,7 @@ class AppWindow(QDialog):
 
         #widgets init
         self.ui.bern1.setChecked(True)
+        self.ui.showBernRes.hide()
         self.formulaChanged(0)
         self.bernOneBorder()
 
@@ -57,7 +75,7 @@ class AppWindow(QDialog):
         self.ui.bern4.toggled[bool].connect(self.bernTwoBorders)
         self.ui.getResult.clicked[bool].connect(self.getRes)
 
-    #Смена формулы
+    #Change widgets for selected formula
     def formulaChanged(self, idx):
         self.curForm = idx
         #show widgets for Bernuli
@@ -83,9 +101,10 @@ class AppWindow(QDialog):
             self.ui.poliK.hide()
             self.ui.poliM.hide()
             self.ui.poliP.hide()
+            self.ui.showBernRes.hide()
 
         #show widgets for Polinomal
-        else:
+        elif idx == 1:
             # hide widgets for Bernuli
             self.ui.groupBern.hide()
             self.ui.groupBern.hide()
@@ -99,6 +118,7 @@ class AppWindow(QDialog):
             self.ui.bernM.hide()
             self.ui.bernM1.hide()
             self.ui.bernM2.hide()
+            self.ui.showBernRes.hide()
             # show widgets for Polinomal
             self.ui.label_11.show()
             self.ui.label_12.show()
@@ -108,8 +128,32 @@ class AppWindow(QDialog):
             self.ui.poliK.show()
             self.ui.poliM.show()
             self.ui.poliP.show()
+        elif idx == 2:
+            # show widgets for Integral Muavra
+            self.ui.FormulaView.setPixmap(QPixmap('Muavra.jpg').scaled(QSize(600, 72)))
+            self.ui.label_6.show()
+            self.ui.label_7.show()
+            self.ui.bernP.show()
+            self.ui.bernN.show()
+            self.ui.label_9.show()
+            self.ui.label_10.show()
+            self.ui.bernM1.show()
+            self.ui.bernM2.show()
+            self.ui.showBernRes.show()
+            # hide widgets for Polinomal and Bernuli
+            self.ui.groupBern.hide()
+            self.ui.label_8.hide()
+            self.ui.bernM.hide()
+            self.ui.label_11.hide()
+            self.ui.label_12.hide()
+            self.ui.label_13.hide()
+            self.ui.label_14.hide()
+            self.ui.poliN.hide()
+            self.ui.poliK.hide()
+            self.ui.poliM.hide()
+            self.ui.poliP.hide()
 
-    #Выбрано событие с одной границей
+    #selected event with one border
     def bernOneBorder(self):
         self.ui.label_8.show()
         self.ui.bernM.show()
@@ -118,7 +162,7 @@ class AppWindow(QDialog):
         self.ui.bernM1.hide()
         self.ui.bernM2.hide()
 
-    #Выбрано событие с двумя границами
+    #selected event with two borders
     def bernTwoBorders(self):
         self.ui.label_8.hide()
         self.ui.bernM.hide()
@@ -127,7 +171,7 @@ class AppWindow(QDialog):
         self.ui.bernM1.show()
         self.ui.bernM2.show()
 
-    #Вычисление по фомуле бернули
+    #Counting by Bernuli formula
     def solveBern(self):
         if self.ui.bern1.isChecked():
             p = self.ui.bernP.value()
@@ -151,7 +195,7 @@ class AppWindow(QDialog):
             m2 = self.ui.bernM2.value()
             self.ui.textResult.setText(str(bernuli(p, n, m2 + 1, m1)))
 
-    # Вычисление по фомуле бернули
+    # Counting by Polinomal formula
     def solvePoli(self):
         n = self.ui.poliN.value()
         k = self.ui.poliK.value()
@@ -161,7 +205,21 @@ class AppWindow(QDialog):
         arrayP = string.split()
         self.ui.textResult.setText(str(polinomal(n, k, arrayM, arrayP)))
 
-    #Получить результат
+    def solveIntegralMuavra(self):
+        p = self.ui.bernP.value()
+        n = self.ui.bernN.value()
+        m1 = self.ui.bernM1.value()
+        m2 = self.ui.bernM2.value()
+        resMuavra = integralMuavra(n, p, m1, m2, 100)
+        string = "Результат по интегральной теореме Муавра - Лапласа: \n" + str(resMuavra)
+        if self.ui.showBernRes.isChecked():
+            resBern = bernuli(p, n, m2 + 1, m1)
+            string += "\nРезультат по формуле Бернулли: \n" + str(resBern)
+            string += "\nРазница между результатами:\n " + str(abs(resMuavra - resBern))
+        self.ui.textResult.setText(string)
+
+
+    #check data from widgets and get result
     def getRes(self):
         if self.curForm == 0:
             # check for errors
@@ -214,6 +272,24 @@ class AppWindow(QDialog):
                 msg.exec_()
             else:
                 self.solvePoli()
+        elif self.curForm == 2:
+            # check for errors
+            if self.ui.bernP.value() > 1 or self.ui.bernP.value() < 0:
+                # show error
+                msg = QMessageBox()
+                msg.setIcon(QMessageBox.Warning)
+                msg.setText('Вероятность события не может быть больше 1 или меньше 0!')
+                msg.exec_()
+            elif self.ui.bernM.value() > self.ui.bernN.value() or self.ui.bernM1.value() > self.ui.bernN.value() or self.ui.bernM2.value() > self.ui.bernN.value():
+                # show error
+                msg = QMessageBox()
+                msg.setIcon(QMessageBox.Warning)
+                msg.setText('Количество появлений события не может быть больше количества испытаний!')
+                msg.exec_()
+            else:
+                self.solveIntegralMuavra()
+
+
 
 
 
